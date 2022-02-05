9bb5747a5866c30f6277d02f9ac2fe797df9f0e3
jampoolG <jampoolgarcia@gmail.com>
2022-02-05 12:13:31 -0300

    added logic api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private url: string = `${environment.url}/upload`;
+
+  constructor(private _http: HttpClient) { }
+
+  SendApiFile(body: FormData): Observable<any>{
+    return this._http.post(this.url, body)
+  }
 }
